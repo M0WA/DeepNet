24524254b81e111cebe36097c2710af794c95709
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2013-04-17 02:16:10 +0000

    restore

---
+++ HtmlParser/src/UrlInserter.h
@@ -0,0 +1,37 @@
+/*
+ * UrlInserter.h
+ *
+ *  Created on: 20.03.2012
+ *      Author: Moritz Wagner
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace database {
+	class DatabaseConnection;
+}
+
+namespace htmlparser {
+
+class DatabaseUrl;
+class UrlInserter {
+private:
+	UrlInserter();
+public:
+	virtual ~UrlInserter();
+
+public:
+	static int ReadURLFile(const std::string& fileName, std::multimap<std::string,std::string>& fileContent);
+
+	static bool InsertURLFile(database::DatabaseConnection* db,const std::multimap<std::string,std::string>& fileContent);
+	static bool ValidateURLFile(database::DatabaseConnection* db,const std::multimap<std::string,std::string>& fileContent);
+
+	static DatabaseUrl ValidateURL(database::DatabaseConnection* db,const std::string& sDomain, const std::string& sUrl, bool dumpUrl = true);
+	static bool InsertURL(database::DatabaseConnection* db,const std::string& sDomain, const std::string& sUrl);
+};
+
+}
