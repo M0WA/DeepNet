87e33a0ac4d1f29a7b82efd29afbde46e66aaf75
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2014-08-30 10:57:24 +0000

    added missing crypto library for ssl

--- Caching/Release/src/subdir.mk
+++ Caching/Release/src/subdir.mk
@@ -89,7 +89,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -D_REENTRANT -I/usr/include/libxml2 -I../../Networking/src -I../../Tools/src -I../../Logging/src -I../../Database/src -I../../Database/generated -I../../Bot/src -I../../Threading/src -I../../HtmlParser/src -O3 -fPIC -fomit-frame-pointer -g -Wall -c -fmessage-length=0 -pthread -rdynamic -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -D_REENTRANT -I/usr/include/libxml2 -I../../Networking/src -I../../Tools/src -I../../Logging/src -I../../Database/src -I../../Database/generated -I../../Bot/src -I../../Threading/src -I../../HtmlParser/src -O3 -fPIC -fomit-frame-pointer -g -Wall -c -fmessage-length=0 -pthread -rdynamic -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
