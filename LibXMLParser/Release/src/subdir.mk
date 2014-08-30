14970f1688e86b71f65c670729bf35bb1eaa8eb5
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2014-08-30 10:59:10 +0000

    added missing crypto library for ssl

--- LibXMLParser/Release/src/subdir.mk
+++ LibXMLParser/Release/src/subdir.mk
@@ -32,7 +32,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/libxml2 -I../../Tools/src -I../../Threading/src -I../../HtmlParser/src -I../../Logging/src -I../../Networking/src -O3 -fPIC -fomit-frame-pointer -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I/usr/include/libxml2 -I../../Tools/src -I../../Threading/src -I../../HtmlParser/src -I../../Logging/src -I../../Networking/src -O3 -fPIC -fomit-frame-pointer -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
