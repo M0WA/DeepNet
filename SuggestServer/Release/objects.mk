36006b65f028f15ca6df97fe9aeea430a5c3af71
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2014-08-30 11:00:08 +0000

    added missing crypto library for ssl

--- SuggestServer/Release/objects.mk
+++ SuggestServer/Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lpthread -lssl -lfl -lCaching -lFastCGIServer -lHtmlParser -lCrawler -lIndexer -lCaching -lHtmlParser -lThreading -lBot -lDatabase -lNetworking -lLogging -lTools -lThreading -lfcgi++ -lfcgi -lmagic -lxml2 -lcurl -licudata -licuuc -licui18n -lmysqlclient_r -lpcre -lhunspell -lpq -lm -lcrypt
+LIBS := -lpthread -lssl -lcrypto -lfl -lCaching -lFastCGIServer -lHtmlParser -lCrawler -lIndexer -lCaching -lHtmlParser -lThreading -lBot -lDatabase -lNetworking -lLogging -lTools -lThreading -lfcgi++ -lfcgi -lmagic -lxml2 -lcurl -licudata -licuuc -licui18n -lmysqlclient_r -lpcre -lhunspell -lpq -lm -lcrypt -lssl -lcrypto -lpthread
 
