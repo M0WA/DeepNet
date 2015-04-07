728cc450d7ab162f1c19f9fb178022177b7783e9
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2015-04-07 19:46:08 +0000

    deprecated old siridia stuff

--- Scripts/webif/staging/common/common.css
+++ Scripts/webif/staging/common/common.css
@@ -1,7 +1,7 @@
 /* ******************************************************
 
-  this is part of the SIRIDIA search engine software
-  Copyright 2012, SIRIDIA GmbH
+  this is part of the DeepNet search engine software
+  Copyright 2015, Moritz Wagner
   Author: Moritz Wagner (moritz.wagner@mo-sys.de)
 
 ********************************************************* */
