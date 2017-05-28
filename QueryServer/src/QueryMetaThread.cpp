04fa06d78ddb579a32992c5608060a0472d39ea3
admin <admin@60921e8f-f7c8-4c3c-9358-9ac8c82962c6>
2017-05-28 20:36:39 +0000

    improved sorting

--- QueryServer/src/QueryMetaThread.cpp
+++ QueryServer/src/QueryMetaThread.cpp
@@ -86,6 +86,10 @@ bool QueryMetaThread::GetUrlsForKeywords(database::SelectResultContainer<databas
 	database::docmetaTableBase::AddInnerJoinRightSideOn_URLSTAGE_ID(select);
 	database::latesturlstagesTableBase::AddInnerJoinLeftSideOn_URLSTAGE_ID(select);
 
+	if(queryProperties.limitSecondLevelDomainID > 0 || queryProperties.limitSubDomainID > 0) {
+		database::latesturlstagesTableBase::AddInnerJoinRightSideOn_URL_ID(select);
+	}
+
 	std::vector<database::WhereConditionTableColumn*> where;
 	database::docmetaTableBase::GetWhereColumnsFor_DICT_ID(
 		database::WhereConditionTableColumnCreateParam(
