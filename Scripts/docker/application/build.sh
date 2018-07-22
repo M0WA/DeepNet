c87e66ffd1b5ca9c8da33a4c294e2f8fb98246c1
Moritz Wagner <moritz.wagner@mo-sys.de>
2018-07-22 17:16:09 +0100

    generic application install target

--- Scripts/docker/application/build.sh
+++ Scripts/docker/application/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-APPLICATION=$1
+APPLICATION="$1"
 APPLICATION_LC=$(echo "${APPLICATION}" | tr '[:upper:]' '[:lower:]')
 
-cat Dockerfile.template | envsubst > Dockerfile
+TEMPLATE_FILE=Dockerfile.template
+if [ -f "Dockerfile.${APPLICATION}.template" ]; then
+	TEMPLATE_FILE="Dockerfile.${APPLICATION}.template"
+fi
+
+cat "${TEMPLATE_FILE}" | envsubst > Dockerfile
 docker build -t deepnet/${APPLICATION_LC}:intermediate .
 docker run --name ${APPLICATION_LC}_intermediate -v ${LOCAL_INSTALL_DIR}/${APPLICATION}:/opt/${APPLICATION}Install:ro deepnet/${APPLICATION_LC}:intermediate cp -r /opt/${APPLICATION}Install /opt/${APPLICATION}
 docker commit ${APPLICATION_LC}_intermediate deepnet/${APPLICATION_LC}:latest
--- Scripts/docker/build.sh
+++ Scripts/docker/build.sh
@@ -2,26 +2,15 @@
 
 source settings.env
 
-echo "building software"
-( cd buildsystem && ./build.sh )
 
 echo "building db"
 ( cd percona && ./build.sh )
 
-echo "building workerbot"
-( cd application && ./build.sh WorkerBot )
+echo "applications software"
+( cd buildsystem && ./build.sh )
 
-echo "building queryserver"
-( cd application && ./build.sh QueryServer )
-
-echo "building suggestserver"
-( cd application && ./build.sh SuggestServer )
-
-echo "building syncserver"
-( cd application && ./build.sh SyncServer )
-
-echo "building deepnettool"
-( cd application && ./build.sh DeepNetTool )
-
-echo "building databasetool"
-( cd application && ./build.sh DatabaseTool )
+APPLICATIONS=("QueryServer" "SyncServer" "WorkerBot" "DeepNetTool" "SuggestServer" "DatabaseTool")
+for APP in "${APPLICATIONS[@]}"; do
+	echo "${APP}: building image"
+	( cd application && ./build.sh "${APP}" )
+done
