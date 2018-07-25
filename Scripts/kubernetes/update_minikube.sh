25b05c76be909f7a8e09f537637c19f491bef99a
Moritz Wagner <moritz.wagner@mo-sys.de>
2018-07-25 22:10:15 +0100

    worksave

--- Scripts/kubernetes/update_minikube.sh
+++ Scripts/kubernetes/update_minikube.sh
@@ -2,6 +2,12 @@
 
 source settings.env
 
+if [ ! -z "$1" ]; then
+	export APPLICATIONS=("$1")
+fi
+
+kubectl create namespace "${NAMESPACE}" >/dev/null 2>&1
+
 for APP in "${APPLICATIONS[@]}"; do
 	if [ -f "${APP}/service.yaml" ]; then
 		echo "${APP}: removing existing services"
