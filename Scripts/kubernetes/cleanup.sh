bacdd3fb754e2510c4d593c0ca085bd965a85aa5
Moritz Wagner <moritz.wagner@mo-sys.de>
2018-07-31 23:15:15 +0100

    added persistent volume for db

--- Scripts/kubernetes/cleanup.sh
+++ Scripts/kubernetes/cleanup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 source settings.env
 
@@ -6,13 +7,5 @@ if [ ! -z "$1" ]; then
 fi
 
 for APP in "${APPLICATIONS[@]}"; do
-	if [ -f "${APP}/service.yaml" ]; then
-		echo "${APP}: removing services"
-		kubectl delete --namespace="${NAMESPACE}" -f "${APP}/service.yaml" >/dev/null 2>&1
-	fi
-
-	if [ -f "${APP}/deployment.yaml" ]; then
-		echo "${APP}: removing deployments"
-		kubectl delete -f "${APP}/deployment.yaml" >/dev/null 2>&1
-	fi
+	app_command "delete" "${APP}"
 done
